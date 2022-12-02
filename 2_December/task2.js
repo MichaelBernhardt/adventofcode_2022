11802bbb26d6774fa1971f2fc3284c3dc8ef68e6
MichaelBernhardt <earthlingmikey@gmail.com>
2022-12-02 10:42:14 +0200

    Refactored task2

--- 2_December/task2.js
+++ 2_December/task2.js
@@ -10,62 +10,39 @@ for (let i = 0; i < array.length; i++) {
     let opponentPlay = string.slice(0, 1);
     let resultedNeeded = string.slice(2, 3);
 
+    // Opponent play     
+    // A = Rock  
+    // B = Paper 
+    // C = Sissors
 
     // LOSS
-    if (opponentPlay === "A" && resultedNeeded === "X") {
-        myPlay = "SISSORS"    
-    }
-
-    if (opponentPlay === "B" && resultedNeeded === "X") {
-        myPlay = "ROCK"
-    }
-
-    if (opponentPlay === "C" && resultedNeeded === "X") {
-        myPlay = "PAPER"
-    }
+    if (resultedNeeded === "X") {
+        if (opponentPlay === "A") myPlay = "SISSORS"
+        if (opponentPlay === "B") myPlay = "ROCK"
+        if (opponentPlay === "C") myPlay = "PAPER"
+    }   
 
     // DRAW
-    if (opponentPlay === "A" && resultedNeeded === "Y") {
-        myPlay = "ROCK"    
-    }
-
-    if (opponentPlay === "B" && resultedNeeded === "Y") {
-        myPlay = "PAPER"
-    }
-
-    if (opponentPlay === "C" && resultedNeeded === "Y") {
-        myPlay = "SISSORS"
+    if (resultedNeeded === "Y") {
+        if (opponentPlay === "A") myPlay = "ROCK"    
+        if (opponentPlay === "B") myPlay = "PAPER"
+        if (opponentPlay === "C") myPlay = "SISSORS"
     }
 
     // WIN
-    if (opponentPlay === "A" && resultedNeeded === "Z") {
-        myPlay = "PAPER"    
-    }
-
-    if (opponentPlay === "B" && resultedNeeded === "Z") {
-        myPlay = "SISSORS"
-    }
-
-    if (opponentPlay === "C" && resultedNeeded === "Z") {
-        myPlay = "ROCK"
-    }
-    
-
-    // A Rock  
-    // B Paper 
-    // C Sissors
-
-    // 0 loss
-    // 3 draw
-    // 6 won
-
-    // X Need to Loose
-    // Y Need to Draw
-    // Z Need to Win
+    if (resultedNeeded === "Z") {
+        if (opponentPlay === "A") myPlay = "PAPER"
+        if (opponentPlay === "B") myPlay = "SISSORS"
+        if (opponentPlay === "C") myPlay = "ROCK"
+    }    
 
     if (myPlay === "ROCK") pointsForMatch = 1 // Rock
     if (myPlay === "PAPER") pointsForMatch = 2 // Paper
     if (myPlay === "SISSORS") pointsForMatch = 3 // Sissors 
+
+    // 0 loss
+    // 3 draw
+    // 6 won
 
     // DRAW
      if ((opponentPlay === "A" && myPlay === "ROCK") || (opponentPlay === "B" && myPlay === "PAPER") || (opponentPlay === "C" && myPlay === "SISSORS")) {
@@ -80,4 +57,4 @@ for (let i = 0; i < array.length; i++) {
     totalPoints = totalPoints + pointsForMatch;
 }
 
-console.log("TotalPoints: " + totalPoints)
+console.log("TotalPoints: " + totalPoints);
